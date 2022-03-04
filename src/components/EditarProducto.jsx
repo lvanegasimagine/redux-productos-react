18c569c92f685583233d71cfdca1fab2c081dd27
Luis Vanegas <lvanegas1429@gmail.com>
2022-03-04 16:45:40 -0600

    Asignando valores a editar en el formulario

--- src/components/EditarProducto.jsx
+++ src/components/EditarProducto.jsx
@@ -1,4 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
+import { obtenerProductoEditar } from "../actions/productoActions";
+
+
 const EditarProducto = () => {
+
+    const producto = useSelector(state => state.productos.productoEditar);
+    if(!producto) return null;
+    const { nombre, precio, id } = producto;
+
     return ( <div className="row justify-content-center">
         <div className="col-md-8">
             <div className="card">
@@ -9,11 +18,11 @@ const EditarProducto = () => {
                     <form action="">
                         <div className="form-group">
                             <label htmlFor="">Nombre Producto</label>
-                            <input type="text" className="form-control" placeholder="Nombre Producto" name="nombre"/>
+                            <input type="text" className="form-control" placeholder="Nombre Producto" name="nombre" value={nombre}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="">Precio Producto</label>
-                            <input type="number" className="form-control" placeholder="Precio Producto" name="precio"/>
+                            <input type="number" className="form-control" placeholder="Precio Producto" name="precio" value={precio}/>
                         </div>
                         <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Guardar Cambios</button>
                     </form>
